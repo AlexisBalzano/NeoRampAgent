b1f7666dbd63976696b1d068c5d96799cdc995f3
Alexis <alexis.balzano@epfl.ch>
2025-10-24 23:24:18 +0200

    Removed https from API url

--- src/NeoRampAgent.h
+++ src/NeoRampAgent.h
@@ -9,7 +9,7 @@
 #include "core/NeoRampAgentCommandProvider.h"
 
 constexpr const char* NEORAMPAGENT_VERSION = "v1.0.0";
-constexpr const char* RAMPAGENT_API = "https://pintade.vatsim.fr";
+constexpr const char* RAMPAGENT_API = "pintade.vatsim.fr";
 
 using namespace PluginSDK;
 
